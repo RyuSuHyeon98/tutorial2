02116c7dad9427bd180fc977aaa610e5f717dd22
RyuSuHyeon98 <114630366+RyuSuHyeon98@users.noreply.github.com>
2022-11-08 13:52:03 +0900

    deploy

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ sex = r2_col2.selectbox("sex", sex_option)
 is_male = sex_option[0] == sex
 
 region_option = ('southwest', 'southeast', 'northwest', 'northeast')
-region = r2_col3.selectbox("region", region_option)
+region = r2_col3.selectbox("region🌏", region_option)
 is_southwest = region_option[0] == region
 is_southeast = region_option[1] == region
 is_northwest = region_option[2] == region
